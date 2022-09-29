2207289db76b7b9f3582f99975298b820e99a8d1
ortap <jjelder@live.com>
2022-09-29 12:36:50 -0700

    Create TestPython
    For Gitstats

---
+++ TestPython.py
@@ -0,0 +1,8 @@
+# This is a sample python code to update for gitstats
+
+import sys
+
+def main():
+    print("Hello from the speaking project!")
+    print("This is a sample python code to update for gitstats")
+    print("From Week 4!")
