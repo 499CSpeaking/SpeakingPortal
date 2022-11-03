4d88d3ec408dbc23ce6cc7bc73ed0fc7197db08d
Colin P <colin.p.pereira@gmail.com>
2022-11-02 17:18:55 -0700

    created base functions for manual implementation, actual functionality pending

--- code/manual_Ipa/main/main.js
+++ code/manual_Ipa/main/main.js
@@ -1,33 +1,27 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import fetch from "node-fetch";
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = require("fs");
 function getDict() {
-    let dict = fs.readFileSync('./dict.txt', 'utf-8');
+    var dict = fs.readFileSync('./dict.txt', {encoding: 'utf8'});
     console.log(dict);
     return dict;
 }
-
 // return ipa associated with word
 function getIpa(word) {
     return word;
 }
-
 function genIpa() {
     // read script and dictionary
-    let script = fs.readFileSync('./script.txt', 'utf-8');
-    let dict = getDict();
-
+    var script = fs.readFileSync('./script.txt', 'utf-8');
+    var dict = getDict();
     // make key>value map 
-    let ipaScript = new Map<string, any>;
-
+    var ipaScript = new Map;
     // for each word in the script, generate a key>value pair that has a
     // word from the script with a phoneme related to that word
-    for (let i = 0; i < script.length; i++) {
+    for (var i = 0; i < script.length; i++) {
         if (dict[i].includes(script[i])) {
             ipaScript.set('ipa' + i, getIpa(script[i]));
         }
     }
 }
-
 getDict();
