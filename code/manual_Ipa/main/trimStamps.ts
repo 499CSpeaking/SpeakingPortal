9e1f69ad96f911d5ad7cb2ff826c6fd4f61dae7b
Colin P <colin.p.pereira@gmail.com>
2023-02-02 13:37:29 -0800

    added trimming function

--- code/manual_Ipa/main/trimStamps.ts
+++ code/manual_Ipa/main/trimStamps.ts
@@ -2,5 +2,28 @@
 
 // algo to strip smaller stamps from list
 function trimStamps(stamps, wordCount) {
- 
+    // set max number of values to be deleted
+    let maxDel = stamps.size-wordCount;
+    let i = 0;
+    // find values to be deleted
+    for (let key of stamps.keys()) {
+        // end condition - if we have deleted enough stamps
+        if (i == maxDel){
+            break;
+        }
+        // get difference between start and end to determine if it is too small
+        let start = stamps.get(key).start;
+        let end = stamps.get(key).end;
+        let diff = end-start;
+        // delete the stamp from the list 
+        if (diff <= 100) {
+            stamps.delete(key);
+            i++;
+        }
+    }
+
+    // return trimmed stamps
+    return stamps;
 }
+
+module.exports = trimStamps;
