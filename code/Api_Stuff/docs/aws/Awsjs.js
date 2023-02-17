6e14130a7b6af1e67d711cef2b00004d10b55d94
ortap <jjelder@live.com>
2023-02-17 12:02:30 -0800

    Final commit

--- code/Api_Stuff/docs/aws/Awsjs.js
+++ code/Api_Stuff/docs/aws/Awsjs.js
@@ -1,40 +1,47 @@
-// Import required AWS SDK clients and commands for Node.js.
-import { PutObjectCommand, CreateBucketCommand } from "@aws-sdk/client-s3";
-import { s3Client } from "./libs/s3Client.js";
+const speech = require('@google-cloud/speech');
 
-// Set the parameters
-const params = {
-  Bucket: "BUCKET_NAME", // The name of the bucket. For example, 'sample-bucket-101'.
-  Key: "KEY", // The name of the object. For example, 'sample_upload.txt'.
-  Body: "BODY", // The content of the object. For example, 'Hello world!".
-};
+const client = new speech.SpeechClient();
 
-const run = async () => {
-  // Create an Amazon S3 bucket.
-  try {
-    const data = await s3Client.send(
-        new CreateBucketCommand({ Bucket: params.Bucket })
-    );
-    console.log(data);
-    console.log("Successfully created a bucket called ", data.Location);
-    return data; // For unit tests.
-  } catch (err) {
-    console.log("Error", err);
-  }
-  // Create an object and upload it to the Amazon S3 bucket.
-  try {
-    const results = await s3Client.send(new PutObjectCommand(params));
-    console.log(
-        "Successfully created " +
-        params.Key +
-        " and uploaded it to " +
-        params.Bucket +
-        "/" +
-        params.Key
-    );
-    return results; // For unit tests.
-  } catch (err) {
-    console.log("Error", err);
-  }
-};
-run();
+/**
+ * Calls the Speech-to-Text API on a demo audio file.
+ */
+async function quickstart() {
+// The path to the remote LINEAR16 file stored in Google Cloud Storage
+  const gcsUri = 'gs://cloud-samples-data/speech/brooklyn_bridge.raw';
+
+  // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+  const audio = {
+    uri: gcsUri,
+  };
+  const config = {
+    encoding: 'LINEAR16',
+    sampleRateHertz: 16000,
+    languageCode: 'en-US',
+    enableWordTimeOffsets: true,
+  };
+  const request = {
+    audio: audio,
+    config: config,
+  };
+
+  // Detects speech in the audio file
+  const [response] = await client.recognize(request);
+  response.results.forEach((result) => {
+    result.alternatives.forEach((alternative) => {
+      console.log(`Transcript: ${alternative.transcript}`);
+      console.log(`Word details:`);
+      console.log(` Word count ${alternative.words.length}`);
+      alternative.words.forEach((item) => {
+        console.log(`  ${item.word}`);
+        const s = parseInt(item.startTime.seconds) +
+         item.startTime.nanos/1000000000;
+        console.log(`   WordStartTime: ${s}s`);
+        const e = parseInt(item.endTime.seconds) +
+        item.endTime.nanos/1000000000;
+        console.log(`   WordEndTime: ${e}s`);
+      });
+    });
+  });
+}
+
+quickstart();
