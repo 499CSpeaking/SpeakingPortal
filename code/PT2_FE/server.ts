f3e967026117ce467007d86e5f1f377030240e95
Colin P <colin.p.pereira@gmail.com>
2023-03-08 16:20:41 -0800

    FE can gen audio from kukarella

--- code/PT2_FE/server.ts
+++ code/PT2_FE/server.ts
@@ -2,9 +2,11 @@
 const express = require("express");
 const server = express();
 const body_parser = require("body-parser");
-const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
-const fs = require("fs");
+import { get } from "https";
+import { argv, exit } from "process";
+import { execSync } from "child_process";
+import { createWriteStream, readFileSync, write, writeFileSync } from "fs";
+import path = require("path");
 
 server.use(body_parser.json());
 server.use(
@@ -20,8 +22,43 @@ server.use(express.urlencoded({ extended: true }));
 // default site url
 server.use("/", express.static("site"));
 
-server.post("/api/", (req, res) => {
-  
+// get audio from kukarella
+server.post("/kuk/", async (req, res) => {
+  const api_url: string = "https://api.kukarella.com/texttospeech/convertTTSPreview";
+
+  const payload =  {
+    text: req.body.inputString,
+    voiceKey: req.body.voiceKey,
+  }
+  console.log("Payload Ready: ", payload);
+
+  const api_respo: any = await fetch(api_url, {
+    method: 'POST',
+    body: JSON.stringify(payload),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+
+  const audio_url: string = (await api_respo.json()).data.url;
+
+  get(audio_url, (res) => {
+    const path = "demo_files/audio.wav";
+    const writeStream = createWriteStream(path);
+
+    res.pipe(writeStream);
+
+    writeStream.on("finish", () => {
+      writeStream.close();
+      console.log("Download Completed");
+      setRes(path);
+    })
+  })
+
+  function setRes(audioPath: string) {
+    console.log("Audio Path: ", audioPath);
+    res.json({audioPath: audioPath});
+  }
 });
 
 
